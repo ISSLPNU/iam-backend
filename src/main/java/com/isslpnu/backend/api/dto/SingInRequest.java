0bceff7aff391db14fc107784f48f217aa550986
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2024-12-04 19:59:24 +0200

    validated captha on sign in

--- src/main/java/com/isslpnu/backend/api/dto/SingInRequest.java
+++ src/main/java/com/isslpnu/backend/api/dto/SingInRequest.java
@@ -1,12 +1,15 @@
 package com.isslpnu.backend.api.dto;
 
+import com.isslpnu.backend.api.dto.captcha.CaptchaRequest;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.net.CacheRequest;
+
 @Getter
 @Setter
-public class SingInRequest {
+public class SingInRequest extends CaptchaRequest {
 
     @NotBlank
     private String email;
--- src/main/java/com/isslpnu/backend/api/validator/SignInValidator.java
+++ src/main/java/com/isslpnu/backend/api/validator/SignInValidator.java
@@ -4,6 +4,7 @@ import com.isslpnu.backend.api.dto.SingInRequest;
 import com.isslpnu.backend.constant.LoginStatus;
 import com.isslpnu.backend.exception.RequestThrottleException;
 import com.isslpnu.backend.security.util.SessionInfo;
+import com.isslpnu.backend.service.CaptchaService;
 import com.isslpnu.backend.service.LoginHistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +19,7 @@ import java.time.LocalDateTime;
 public class SignInValidator implements Validator {
 
     private final LoginHistoryService loginHistoryService;
+    private final CaptchaService captchaService;
 
     @Value("${auth.max_attempts}")
     private int maxAttempts;
@@ -31,11 +33,13 @@ public class SignInValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
+        SingInRequest request = (SingInRequest) target;
         String ip = SessionInfo.getIp();
         LocalDateTime dateTime = LocalDateTime.now().minusMinutes(period);
         int counter = loginHistoryService.countByIpAndStatusAndCreatedAtAfter(ip, LoginStatus.FAILURE, dateTime);
         if (counter > maxAttempts) {
             throw new RequestThrottleException("Too many sign in requests :)");
         }
+        captchaService.verifyCaptcha(request);
     }
 }
